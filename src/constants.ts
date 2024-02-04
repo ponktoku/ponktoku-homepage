68dc36e7e8169c9d95cd32896a0149af78213209
Danny Bao <88077806+DannyBoy5930@users.noreply.github.com>
2024-02-04 23:15:08 +1100

    Implement flowbite navbar and darkmode with transparency and logo

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const SITE_TITLE = 'Danny Bao';
+export const SITE_DESCRIPTION = "Danny Bao's Portfolio";
+export const HOMEPAGE_ARTICLE_LIMIT = 6;
+export const ARTICLES_PER_PAGE = 6;
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: [
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+		'./node_modules/flowbite/**/*.js'
+	],
+	darkMode: "class",	
 	theme: {
-		extend: {},
+		colors: {
+			transparent: 'transparent',
+			current: 'currentColor',
+			// dark mode
+			'dm-white': '#eeeeee',
+			'dm-blue-pastel': '#88ccca',
+			'dm-dark-gray': '#202023',
+			'dm-yellow': '#FBD38D',
+			'dm-yellow-highlight': '#f6ad55',
+			'dm-pink': '#EB5DB4',
+			'dm-highlight-box': '#313134',
+			// light mode
+			'lm-purple': '#805AD5',
+			'lm-purple-highlight': '#6b46c1',
+			'lm-blue': '#3D7AED',
+			'lm-green': '#2C7A7B',
+			'lm-text-color': '#1A202C',
+			'lm-bkg': '#F0E7DB',
+			'lm-navbar-bkg': '#f4ede4',
+		},
+		extend: {
+		},
 	},
-	plugins: [],
+	plugins: [
+		require('flowbite/plugin')
+	],
 }
