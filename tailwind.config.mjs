0b9ca3bb85e9f1a0793ab9825436d825e364f494
Danny Bao <88077806+DannyBoy5930@users.noreply.github.com>
2024-02-09 00:21:36 +1100

    Fill out Homepage, make components, add sharp package for Images, and create other pages for navbar links

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,7 @@
 export default {
 	content: [
 		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
-		'./node_modules/flowbite/**/*.js'
+		'./node_modules/flowbite/**/*.js',
 	],
 	darkMode: "class",	
 	theme: {
@@ -17,11 +17,15 @@ export default {
 			'dm-yellow-highlight': '#f6ad55',
 			'dm-pink': '#EB5DB4',
 			'dm-highlight-box': '#313134',
+			'dm-blue-pastel-highlight': '#2c3839',
+			'dm-button-hover': '#4fd1c5',
+			'dm-green-button': '#81E6D9',
 			// light mode
 			'lm-purple': '#805AD5',
 			'lm-purple-highlight': '#6b46c1',
 			'lm-blue': '#3D7AED',
-			'lm-green': '#2C7A7B',
+			'lm-green': '#319795',
+			'lm-green-hover': '#2C7A7B',
 			'lm-text-color': '#1A202C',
 			'lm-bkg': '#F0E7DB',
 			'lm-navbar-bkg': '#f4ede4',
