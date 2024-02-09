572c96526718cef25d8656ed1c976c985d1a26b3
Danny Bao <88077806+DannyBoy5930@users.noreply.github.com>
2024-02-09 22:49:55 +1100

    Add mdx integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import preact from "@astrojs/preact";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact()],
+  integrations: [tailwind(), preact(), mdx()],
   server: "hybrid",
 });
