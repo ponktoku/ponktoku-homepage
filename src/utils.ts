4207210aa6679cab4a7ea18cbf532d3f038a1a90
Danny Bao <88077806+DannyBoy5930@users.noreply.github.com>
2024-02-11 17:34:06 +1100

    Add ArticleCard and Tags components, with example .md/.mdx files for All Post's page

---
+++ src/utils.ts
@@ -0,0 +1,16 @@
+// Format the date to a string
+function formatDate(date: Date): string {
+  const options: Intl.DateTimeFormatOptions = {year: 'numeric', month: 'long', day: 'numeric'};
+
+  return new Date(date).toLocaleDateString(undefined, options);
+}
+
+// Capitalise the first letter
+function capitalise(str: string): string {
+  if (typeof str !== 'string' || str.length === 0) {
+    return str;
+  }
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+export { formatDate, capitalise };
