d051d0ad91e81d31667036099d181ce191f7bb23
Danny Bao <88077806+DannyBoy5930@users.noreply.github.com>
2024-02-20 17:19:31 +1100

    Add astro-expressive-code integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
-
 import mdx from "@astrojs/mdx";
+
+import expressiveCode from "astro-expressive-code";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact(), mdx()],
-  output: "static",
+  integrations: [tailwind(), preact(), mdx(), expressiveCode()],
+  output: "static"
 });
