c2f6c65ee04ab494ee0305713a63fbe2eeef55ee
Danny Bao <88077806+DannyBoy5930@users.noreply.github.com>
2024-02-20 17:34:23 +1100

    Remove Preact integration from astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import preact from "@astrojs/preact";
 import mdx from "@astrojs/mdx";
 
 import expressiveCode from "astro-expressive-code";
