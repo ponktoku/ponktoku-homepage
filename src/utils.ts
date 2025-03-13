0926c8d1b71fe4f2bc5e8683034ddcf1c5700002
Danny Bao <88077806+DannyBoy5930@users.noreply.github.com>
2025-03-13 19:24:48 +1100

    chore: Lint utils file

--- src/utils.ts
+++ src/utils.ts
@@ -1,19 +1,23 @@
 // Format the date to a string
 function formatDate(date: Date): string {
-  const options: Intl.DateTimeFormatOptions = {year: 'numeric', month: 'long', day: 'numeric'};
+  const options: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
 
   return new Date(date).toLocaleDateString(undefined, options);
 }
 
 // Capitalise the first letter
 function capitalise(str: string): string {
-  if (typeof str !== 'string' || str.length === 0) {
+  if (typeof str !== "string" || str.length === 0) {
     return str;
   }
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
-function capitaliseAll(str: string):string {
+function capitaliseAll(str: string): string {
   return str.toUpperCase();
 }
 
